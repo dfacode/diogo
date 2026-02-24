d3ac2ff5f89e79765fa5041244e5217f62c041cd
dfacode <d.dfalves@gmail.com>
2026-02-24 15:32:23 -0300

    Translate Texts PT / EN

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -1,15 +1,27 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent"
+], function(Controller, UIComponent) {
+    "use strict";
 
-        return Controller.extend("diogo.controller.View", {
-            onInit: function () {
-
+    return Controller.extend("diogo.controller.View", {
+        onInit: function() {
+            // Initialize i18n model if needed
+            var oModel = this.getOwnerComponent().getModel("i18n");
+            if (!oModel) {
+                var oResourceModel = new sap.ui.model.resource.ResourceModel({
+                    bundleName: "diogo.i18n.i18n"
+                });
+                this.getOwnerComponent().setModel(oResourceModel, "i18n");
             }
-        });
+        },
+
+        onLanguageToggle: function() {
+            var oCore = sap.ui.getCore();
+            var sCurrentLanguage = oCore.getConfiguration().getLanguage();
+            var sNewLanguage = sCurrentLanguage.includes("pt") ? "en" : "pt_BR";
+            
+            oCore.getConfiguration().setLanguage(sNewLanguage);
+        }
     });
+});
