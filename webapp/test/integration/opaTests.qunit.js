a666853f0a8efe45bff369195ebf2ba3e1247022
dfacode <d.dfalves@gmail.com>
2026-02-24 14:30:00 -0300

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["diogo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"diogo/test/unit/controller/View.controller"
+], function () {
+	"use strict";
+});
